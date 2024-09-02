85564a4de0d1307fac8cab5dd7392bf284082ec2
Jesung Yang <y.j3ms.n@gmail.com>
2024-09-02 09:54:48 +0900

    backup

---
+++ src/run-Oz.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-Oz-blender_r-526.sh
+
+./asm_gen_non-mod-Oz.sh
+./asm_gen_mod-Oz.sh
+
+./compare_s.sh > result-Oz
---
+++ src/run.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-blender_r-526.sh
+
+./asm_gen_non-mod.sh
+./asm_gen_mod.sh
+
+./compare_s.sh > result-O3
